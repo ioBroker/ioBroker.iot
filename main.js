68da6ce99c16723077a2b3f27e53bdee1c1afa16
GermanBluefox <dogafox@gmail.com>
2016-06-19 14:57:34 +0200

    (bluefox9 working verison

--- main.js
+++ main.js
@@ -15,14 +15,10 @@ var adapter = utils.adapter({
         if (socket) socket.emit('objectChange', id, obj);
     },
     stateChange: function (id, state) {
-        if (id === 'hm-rpc.0.JEQ0066707.1.STATE') {
-            console.log(state.val);
-        }
         if (socket) socket.emit('stateChange', id, state);
     },
     unload: function (callback) {
         try {
-            if (webServer) webServer.io.close();
             if (socket) socket.close();
             ioSocket = null;
             callback();
@@ -36,15 +32,21 @@ var adapter = utils.adapter({
 });
 
 function main() {
+    //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
     if (!adapter.config.apikey) {
-        adapter.log.error('No api-key found. Please get one on https://cloud.iobroker.net');
+        adapter.log.error('No api-key found. Please get one on https://iobroker.net');
         process.stop();
         return;
     }
     
     adapter.setState('info.connection', false, true);
-    
-    socket = require('socket.io-client')('http://localhost:10555');
+    adapter.config.cloudUrl  = adapter.config.cloudUrl || 'https://iobroker.net:10555';
+
+    adapter.log.info('Connecting with ' + adapter.config.cloudUrl + ' with "' + adapter.config.apikey + '"');
+    socket = require('socket.io-client')(adapter.config.cloudUrl || 'https://iobroker.net:10555', {
+        rejectUnauthorized: !adapter.config.allowSelfSignedCertificate
+    });
     
     socket.on('connect', function () {
         adapter.log.info('Connection changed: CONNECTED');
@@ -56,17 +58,37 @@ function main() {
         adapter.setState('info.connection', false, true);
     });
 
-    socket.on('error', function(error){
+    socket.on('error', function (error){
+        adapter.log.error('Connection error: ' + error);
         console.log('error: ' + error);
     });
     
     var server = 'http://localhost:8082';
-
     socket.on('html', function (url, cb) {
         request({url: server + url, encoding: null}, function (error, response, body) {
             cb(error, response.statusCode, response.headers, body);
         });
     });
 
-    ioSocket = new IOSocket(socket, {clientid: adapter.config.apikey}, adapter);
+    if (adapter.config.instance) {
+        if (adapter.config.instance.substring(0, 'system.adapter.'.length) !== 'system.adapter.') {
+            adapter.config.instance = 'system.adapter.' + adapter.config.instance;
+        }
+
+        adapter.getForeignObject(adapter.config.instance, function (err, obj) {
+            if (obj) {
+                server = 'http' + (obj.native.secure ? 's' : '')  + '://';
+                // todo if run on other host
+                server += (!obj.native.bind || obj.native.bind === '0.0.0.0') ? '127.0.0.1' : obj.native.bind;
+                server += ':' + obj.native.port;
+
+                ioSocket = new IOSocket(socket, {clientid: adapter.config.apikey}, adapter);
+            } else {
+                adapter.log.error('Unknown instance ' + adapter.log.instance);
+                throw new Error('Unknown instance ' + adapter.log.instance);
+            }
+        });
+    } else {
+        ioSocket = new IOSocket(socket, {clientid: adapter.config.apikey.trim()}, adapter);
+    }
 }
