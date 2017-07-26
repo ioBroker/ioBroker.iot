3147277dc3b65b34875d42d20c447d3b30e64b68
Chris Klinger <git@chrisklinger.de>
2017-07-26 20:45:08 +0200

    Added extended logging for connection errors

--- main.js
+++ main.js
@@ -1383,6 +1383,10 @@ function connect() {
         reconnectionDelayMax: 10000
     });
 
+    socket.on('connect_error', (error) => {
+      adapter.log.error('Error while connecting to cloud: ' + error);
+    });
+
     socket.on('connect', function () {
         if (!connected) {
             adapter.log.info('Connection changed: CONNECTED1');
