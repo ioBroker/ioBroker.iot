68b56455d0d9b9baa8b26144972a9b5f5343bb5e
Philipp Beckers <github@grimneko.de>
2017-11-26 19:50:06 +0100

    added missing translations/improved translations, wording/fixed typos/refined sentenses

--- admin/words.js
+++ admin/words.js
@@ -8,12 +8,12 @@ systemDictionary = {
     "Cloud URL:":   {"en": "Cloud URL:",    "de": "Cloud URL:",             "ru": "Адрес облака:"},
     "Allow self-signed certificates": {
         "en": "Allow self-signed certificates",
-        "de": "Erlaube self-sigend Zertifikate",
+        "de": "Erlaube selbst-signierte Zertifikate",
         "ru": "Разрешить самоподписанные сертификаты"
     },
     "help_tip": {
         "en": 'No APP-KEY? Get it on <a href="https://iobroker.pro/accessRemote" target="_blank">https://iobroker.pro</a> for validated access or on <a href="https://iobroker.net" target="_blank">https://iobroker.net</a> for free, but limited access',
-        "de": 'Kein APP-KEY? Bekomme den unter <a href="https://iobroker.pro/accessRemote" target="_blank">https://iobroker.pro</a> für geprüffte Zugirff oder kostenlos unter <a href="https://iobroker.net" target="_blank">https://iobroker.net</a> (begrenzt)',
+        "de": 'Kein APP-KEY? Bekomme den unter <a href="https://iobroker.pro/accessRemote" target="_blank">https://iobroker.pro</a> für geprüften Zugriff oder kostenlos (aber begrenzteren) unter <a href="https://iobroker.net" target="_blank">https://iobroker.net</a>',
         "ru": 'Нет APP-KEY? Получи его на <a href="https://iobroker.pro/accessRemote" target="_blank">https://iobroker.pro</a> для гарантированного доступа или бесплатно на <a href="https://iobroker.net" target="_blank">https://iobroker.net</a>, но с ограниченным доступом'
     },
     "Language:":    {"en": "Language:",     "de": "Sprache:",               "ru": "Язык:"},
@@ -22,7 +22,7 @@ systemDictionary = {
     "Smart Devices": {"en": "Smart Devices", "de": "Smart Geräte",          "ru": "Smart устройства"},
     "Smart Enums":  {"en": "Smart Enums",   "de": "Smart Aufzählungen",     "ru": "Smart категории"},
     "by ON":        {"en": "by ON",         "de": "bei AN",                 "ru": "при ВКЛ"},
-    "last value":   {"en": "last value",    "de": "leztes",                 "ru": "последн."},
+    "last value":   {"en": "last value",    "de": "letzter Wert",                 "ru": "последн."},
     "Allow self-signed certificates:": {
         "en": "Allow self-signed certificates:",
         "de": "Erlaube selbst-signierte Zertifikate:",
@@ -30,12 +30,12 @@ systemDictionary = {
     },
     "Use following link for IFTTT:": {
         "en": "Use following link for IFTTT:",
-        "de": "Benutze folgendes Link für IFTTT:",
+        "de": "Benutze folgenden Link für IFTTT:",
         "ru": "Используйте этот URL для IFTTT:"
     },
     "Use following link for custom service:": {
         "en": "Use following link for custom service:",
-        "de": "Benutze folgendes Link für eigenen Service:",
+        "de": "Benutze folgende Link für einen eigenen Service:",
         "ru": "Используйте этот URL для своего сервиса:"
     },
     "Use text2command instance:": {
@@ -61,14 +61,14 @@ systemDictionary = {
     },
     "Active":               {"en": "Active",                "de": "aktiv",                 "ru": "Активно"},
     "Smart Media":          {"en": "Smart Media",           "de": "Smart Media",            "ru": "Smart Media"},
-    "Extended options":     {"en": "Extended options",      "de": "Extra-Einstellungen",    "ru": "Расширенные настройки"},
+    "Extended options":     {"en": "Extended options",      "de": "Erweiterte Einstellungen",    "ru": "Расширенные настройки"},
     "Services and IFTTT":   {"en": "Services and IFTTT",    "de": "Services und IFTTT",     "ru": "Сервисы и IFTTT"},
     "White list for services:": {"en": "White list for services:", "de": "White list für Services:", "ru": "Белый список для сервисов:"},
     "Timeout:":             {"en": "Connection timeout(ms):",   "de": "Verbindungs-Timeout(ms):",   "ru": "Таймаут соединения(мс):"},
     "disabled":     {"en": "disabled",      "de": "deaktiviert",            "ru": "неактивно"},
     "Smart name:":  {"en": "Smart name:",   "de": "Smart-Name:",            "ru": "Smart имя:"},
-    "Rooms":        {"en": "Rooms",         "de": "Rooms",                  "ru": "Комнаты"},
-    "Functions":    {"en": "Functions",     "de": "Functions",              "ru": "Функции"},
+    "Rooms":        {"en": "Rooms",         "de": "Räume",                  "ru": "Комнаты"},
+    "Functions":    {"en": "Functions",     "de": "Funktionen",              "ru": "Функции"},
     "Ok":           {"en": "Ok",            "de": "Ok",                     "ru": "Ok"},
     "Cancel":       {"en": "Cancel",        "de": "Abbrechen",              "ru": "Отмена"},
     "Smart name":   {"en": "Smart name",    "de": "Smartname",              "ru": "Smart имя"},
@@ -79,18 +79,18 @@ systemDictionary = {
     "Off":          {"en": "off",           "de": "aus",                    "ru": "выкл"},
     "Filter: ":     {"en": "Filter: ",      "de": "Filter: ",               "ru": "Фильтр: "},
     "Error":        {"en": "Error",         "de": "Fehler",                 "ru": "Ошибка"},
-    "Replace in names:": {"en": "Replace in names:", "de": "Ersetzte in Namen:", "ru": "Заменить в именах:"},
+    "Replace in names:": {"en": "Replace in names:", "de": "Ersetze in Namen:", "ru": "Заменить в именах:"},
     "IFTTT key:":   {"en": "IFTTT key:",    "de": "IFTTT key:",             "ru": "IFTTT key:"},
     "Are you sure?": {"en": "Are you sure?", "de": "Sind Sie sicher?",      "ru": "Уверены?"},
     "OFF level for switches:":  {"en": "OFF level for switches:",   "de": "OFF level for switches:",            "ru": "OFF level for switches:"},
-    "modified":                 {"en": "Name was modified by user", "de": "Der Name war vom Anwender geändert", "ru": "Имя изменено пользователем"},
+    "modified":                 {"en": "Name was modified by user", "de": "Der Name wurde vom Anwender geändert", "ru": "Имя изменено пользователем"},
     "Edit smart name":          {"en": "Edit smart name",           "de": "Smartname editieren",                "ru": "Редактировать имя"},
     "Write response to:":       {"en": "Write response to:",        "de": "Schreibe Antwort ins:",              "ru": "Писать ответ в:"},
-    "Duplicate name":           {"en": "Duplicate name",            "de": "Name ist schon benutzt",             "ru": "Имя уже используется"},
-    "Restart on disconnect:":   {"en": "Restart on disconnect:",    "de": "Neustart bei dem Verbindungsabbruch:", "ru": "Перезапуск при потере сединения:"},
+    "Duplicate name":           {"en": "Duplicate name",            "de": "Name wurde schon benutzt",             "ru": "Имя уже используется"},
+    "Restart on disconnect:":   {"en": "Restart on disconnect:",    "de": "Neustart bei Verbindungsabbruch:", "ru": "Перезапуск при потере сединения:"},
     "amazon link": {
-        "en": "If you use Alexa Skill, please make a review for us <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/create-review/?ref-suffix=war_dp' target='_blank' class='here'>here</a>. Examples can be found <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/reviews' target='_blank'>here</a>.",
-        "de": "Falls du Alexa benutzst, bitte eine Review für ioBroker Skill <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/create-review/?ref-suffix=war_dp' target='_blank' class='here'>hier</a> hinterlassen. Beispiele kann man <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/reviews' target='_blank'>hier</a> anschauen.",
+        "en": "If you use the Alexa Skill, please write a review for us <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/create-review/?ref-suffix=war_dp' target='_blank' class='here'>here</a>. Examples can be found <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/reviews' target='_blank'>here</a>.",
+        "de": "Falls du den Alexa Skill benutzt, schreibe doch bitte <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/create-review/?ref-suffix=war_dp' target='_blank' class='here'>hier</a> hier eine Review. Beispiele kann man <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/reviews' target='_blank'>hier</a> anschauen.",
         "ru": "Если вы используете Alexa, пожалуйста оставьте свой отзыв <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/create-review/?ref-suffix=war_dp' target='_blank' class='here'>здесь</a>. Примеры отзывов можно посмотреть <a href='http://alexa.amazon.de/spa/index.html#skills/dp/B01MRXCC3J/reviews' target='_blank'>здесь</a>."
     },
     "info_iftttKey": {
@@ -100,7 +100,7 @@ systemDictionary = {
     },
     "info_allowedServices": {
         "en": "divided by comma. Use * to allow all services.",
-        "de": "mit Komma getrennt. Benutze * um alle Services zu erlauben.",
+        "de": "durch Komma getrennt. Benutze * um alle Services zu erlauben.",
         "ru": "разделённые запятой. Используйте *, что бы разрешить все сервисы."
     },
     "Instance does not exists": {
@@ -114,7 +114,7 @@ systemDictionary = {
         "ru": "Недействительный объект"
     },
     "Authentication must be disabled. You can create second instance extra for cloud.": {
-        "en": "Authentication must be disabled. You can create second instance extra for cloud.",
+        "en": "Authentication must be disabled. You can create a second instance extra for the cloud.",
         "de": "Die Authentifizierung muss deaktiviert sein. Sie können zweite Instanz extra für Cloud erstellen.",
         "ru": "Аутентификация должна быть отключена. Вы можете создать дополнительный экземпляр для облака."
     },
@@ -130,7 +130,7 @@ systemDictionary = {
     },
     "(you can separate names with comma)": {
         "en": "You can enter several names divided by comma",
-        "de": "Sie können mehrere Namen eingeben, geteilt durch Komma",
+        "de": "Sie können mehrere kommaseparierte Namen eingeben",
         "ru": "Вы можете ввести несколько имен, разделенных запятой"
     },
     "Place function in names first:": {
@@ -150,7 +150,7 @@ systemDictionary = {
     },
     "Device will be disabled.": {
         "en": "Device will be disabled.",
-        "de": "Gerät wird aus der Liste verschwinden.",
+        "de": "Gerät wird aus der Liste entfernt.",
         "ru": "Устройство исчезнет из списка."
     },
     "Device will be deactivated.": {
@@ -160,52 +160,52 @@ systemDictionary = {
     },
     "(Set to 0 if behavior not desired)": {
         "en": "(Set to 0 if behavior not desired)",
-        "de": "(Auf 0 setzten, falls das Verhalten nicht erwünscht)",
+        "de": "(Auf 0 setzten, falls das Verhalten unerwünscht ist)",
         "ru": "(для деактивации установить на 0)"
     },
     "tooltip_apikey": {
         "en": "This key is required to communicate with a cloud. You can get the API-KEY on https://iobroker.net and it looks like nickname_f1f439a0-001a-11e7-bc64-92361f002671",
-        "de": "Dieser Schlüssel ist nötig um mit dem Cloud zu kommunizieren. Man kann den Schlüßel unter https://iobroker.net bekommen. Es sieht so aus nickname_f1f439a0-001a-11e7-bc64-92361f002671",
+        "de": "Dieser Schlüssel wird benötig um mit der Cloud zu kommunizieren. Man kann den Schlüssel unter https://iobroker.net bekommen. Es sieht so aus nickname_f1f439a0-001a-11e7-bc64-92361f002671",
         "ru": "Ключ необходимый для коммуникации с облаком. Его можно получить на https://iobroker.net и он похож на nickname_f1f439a0-001a-11e7-bc64-92361f002671"
     },
     "tooltip_instance": {
         "en": "Web instance, which will be shown for your connections from cloud.",
-        "de": "Web Instanz, die für ihre Anfrgaen aus dem Cloud angezeigt wird.",
+        "de": "Web Instanz, die für ihre Anfragen aus dem Cloud angezeigt wird.",
         "ru": "Экземпляр WEB, который будет показываться при доступе из облака"
     },
     "tooltip_cloudUrl": {
-        "en": "Normally you do not need to change it. Only if you use your own cloud.",
-        "de": "Normallerweise muss man diese Option nicht ändern. Nur wenn man eigenen Cloud verwendet.",
+        "en": "Normally you do not need to change it. Only required if you plan to use your own cloud.",
+        "de": "Normallerweise muss man diese Option nicht geändert werden. Dies ist nur notwendig, wenn man eigenen Cloud verwendet.",
         "ru": "Обычно не нужно менять этот параметр. Только если используется собственное облако."
     },
     "tooltip_allowSelfSignedCertificate": {
-        "en": "If you use your own cloud, you can allow to use self-signed certificates for the cloud.",
-        "de": "Wenn man eigenen Cloud verwendet, man kann die Benutzen von self-sigend Zertifikaten erlauben.",
+        "en": "If you use your own cloud, you can allow the use of self-signed certificates for the cloud.",
+        "de": "Wenn man eine eigene Cloud verwendet, man kann die Benutzung von selbst-signierten Zertifikaten erlauben.",
         "ru": "Если используется собственное облако, то можно разрешить использование самоподписанных сертификатов"
     },
     "tooltip_language": {
-        "en": "You can define specific language to switch fast between US Alexa Skill and DE Alexa Skill.",
+        "en": "You can define the languages to switch fast between US Alexa Skill and DE Alexa Skill.",
         "de": "Man kann hier die Sprache definieren um zwischen US Alexa Skill und DE Alexa Skill umschalten zu können",
         "ru": "Можно выбрать язык, что бы быстро переключаться между US Alexa Skill и DE Alexa Skill"
     },
     "tooltip_functionFirst": {
-        "en": "Normally the names looks like room_function. You can switch it to function_room.",
-        "de": "Normallerweise die Auto-Namen sehen wie Raum-Funktion aus. Damit kann man den auf Funktion-Raum ändern.",
+        "en": "Normally the names looks like room_function. Here you can switch it to function_room.",
+        "de": "Die automatischen Namen werden im Schema Raum-Funktion generiert. Hiermit kann man es auf Funktion-Raum geändert werden.",
         "ru": "Обычно имена выглядят как Комната-Функция. Этой настройкой можно переключить на Функция-Комната."
     },
     "tooltip_concatWord": {
         "en": "Define how to concatenate Room and Function. E.g. with 'in': Light in living room.",
-        "de": "Damit wird definiert wie man Funktion und Raum miteinander verbindet. Z.B. mit 'im': Licht im Schlafzimmer",
+        "de": "Damit wird definiert wie Funktion und Raum miteinander verbunden werden; z.B. mit 'im': Licht im Schlafzimmer",
         "ru": "Можно указать каким словом соединять Функцию и Комнату. Например с 'в': Свет в зале."
     },
     "tooltip_deviceOffLevel": {
         "en": "Some groups consist of mixed devices: dimmers and switches. It is allowed to control them with ON and OFF commands and with percents",
-        "de": "Some groups consist of mixed devices: dimmers and switches. It is allowed to control them with ON and OFF commands and with percents",
+        "de": "Gruppen können aus gemischten Geräten (Schaltern und Dimmern) bestehen. Sie können mit ON/OFF sowie Prozentwerten angesteuert werden, hier kann definiert werden welcher Prozentsatz OFF entspricht.",
         "ru": "Некоторые группы состоят из смешанных устройств: диммеров и переключателей. Разрешено управлять ими с помощью команд ON и OFF и с процентами"
     },
     "tooltip_responseOID": {
         "en": "For every command the text response will be generated. You can define here the Object ID , where this text must be written to. E.g. sayit.0.tts.text.",
-        "de": "For every command the text response will be generated. You can define here the Object ID , where this text must be written to. E.g. sayit.0.tts.text.",
+        "de": "Für jedes Kommando wird eine Textantwort generiert. Hier kannst du die Objekt ID angeben, an welchde die Textantword gesendet werden soll, z.B. sayit.0.tts.text.",
         "ru": "Для каждой команды будет генерироваться текстовый ответ. Вы можете определить здесь ID объекта , где этот текст должен быть записан. Например sayit.0.tts.text."
     },
     "tooltip_restartOnDisconnect": {
@@ -215,7 +215,7 @@ systemDictionary = {
     },
     "tooltip_noCommon": {
         "en": "If enabled, the settings will be stored only for this instance. So in the another instance the other names could be set.",
-        "de": "Falls aktiviert, dann werden die Einstellungen nur für diese Instanz gespeichert. Damit kann man in einer anderen Instanz andere Namen nehmen.",
+        "de": "Wenn diese Option aktiviert wird, so werden die Einstellungen nur für diese Instanz gespeichert. So ist es möglich in einer anderen Instanz andere Namen zu setzen.",
         "ru": "Если выбрана эта опция, то настройки будут сохраняться только для этой инстанции. Что бы в другой инстанции можно было выбрать другие имена."
     }
 };
