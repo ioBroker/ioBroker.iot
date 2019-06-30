31b2859468f7931925af15b7fda69e9cf0e807df
GermanBluefox <dogafox@gmail.com>
2019-06-30 17:48:54 +0200

    fix error

--- main.js
+++ main.js
@@ -574,7 +574,7 @@ function startDevice(clientId, login, password, retry) {
                         return adapter.log.error('Cannot convert request: ' + request);
                     }
                     adapter.log.debug('Data: ' + request);
-                    
+
                     if (request && request.alisa) {
                         yandexAlisa && yandexAlisa.process(request, adapter.config.yandexAlisa, response =>
                             device.publish('response/' + clientId + '/' + type, JSON.stringify(response)));
@@ -588,7 +588,7 @@ function startDevice(clientId, login, password, retry) {
 
                         adapter.log.debug(new Date().getTime() + ' ALEXA: ' + JSON.stringify(request));
 
-                        else if (request && request.directive) {
+                        if (request && request.directive) {
                             alexaSH3 && alexaSH3.process(request, adapter.config.amazonAlexa, response =>
                                 device.publish('response/' + clientId + '/' + type, JSON.stringify(response)));
                         } else
