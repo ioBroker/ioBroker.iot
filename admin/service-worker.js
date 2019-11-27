b896bedd4080c7412d3c5ee13c3b362e5cf377ca
GermanBluefox <dogafox@gmail.com>
2019-11-27 20:52:36 +0100

    ### 1.1.9 (2019-11-27) * (bluefox) Fixed: sometimes the configuration could not be loaded

--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1bdb4d94c9bb79beb117b2e1a0a9136e.js"
+  "./precache-manifest.7170f14f5bdf0d479ef1922763b18948.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
