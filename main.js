0d62f57f9b0841b2caf6032c12b425e399c5fc7c
GermanBluefox <dogafox@gmail.com>
2023-05-24 13:46:49 +0200

    Added detection of smart names

--- main.js
+++ main.js
@@ -159,7 +159,6 @@ function startAdapter(options) {
                             adapter.log.info('Request V3 devices');
                             if (alexaSH3) {
                                 adapter.sendTo(obj.from, obj.command, alexaSH3.getDevices(), obj.callback);
-                                adapter.setState('smart.updates3', false, true);
                                 await adapter.setStateAsync('smart.updates3', false, true);
                             } else {
                                 adapter.sendTo(obj.from, obj.command, {error: 'not activated'}, obj.callback);
