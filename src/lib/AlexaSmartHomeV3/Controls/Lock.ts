f31ff798cbb6b009549e6363111a8f6c7089e1df
GermanBluefox <dogafox@gmail.com>
2025-11-03 13:03:11 +0100

    Changed the meaning of LOCK: https://github.com/ioBroker/ioBroker.iot/issues/1062

--- src/lib/AlexaSmartHomeV3/Controls/Lock.ts
+++ src/lib/AlexaSmartHomeV3/Controls/Lock.ts
@@ -1,7 +1,7 @@
 import Capabilities from '../Alexa/Capabilities';
 import Properties from '../Alexa/Properties';
 import Control from './Control';
-import type { AlexaV3Category, IotExternalPatternControl } from '../types';
+import type { AlexaV3Category, AlexaV3DirectiveValue, IotExternalPatternControl } from '../types';
 import type { ControlStateInitObject } from '../Alexa/Properties/Base';
 import EndpointHealth from '../Alexa/Capabilities/EndpointHealth';
 
@@ -27,10 +27,10 @@ export default class Lock extends Control {
         return {
             setState: this.states[map.set]!,
             getState: this.states[map.actual]!,
-            alexaSetter: function (alexaValue) {
-                return alexaValue === Properties.LockState.UNLOCK;
+            alexaSetter: function (alexaValue: AlexaV3DirectiveValue): ioBroker.StateValue | undefined {
+                return alexaValue === Properties.LockState.LOCK;
             },
-            alexaGetter: function (value) {
+            alexaGetter: function (value: ioBroker.StateValue | undefined): AlexaV3DirectiveValue {
                 return value ? Properties.LockState.LOCKED : Properties.LockState.UNLOCKED;
             },
         };
