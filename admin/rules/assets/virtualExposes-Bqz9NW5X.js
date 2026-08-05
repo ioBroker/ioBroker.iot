ea96934e5fb032057ea7085b3aeea98adbf805ed
GermanBluefox <dogafox@gmail.com>
2026-08-05 16:58:37 +0300

    Migrated to TypeScript 6 and react 19

---
+++ admin/rules/assets/virtualExposes-Bqz9NW5X.js
@@ -0,0 +1,1 @@
+import{t as e}from"./virtual_mf-exposes___mfe_internal__ActionVisu__customRuleBlocks_js-BT4JMo4r.js";export{e as default};
---
+++ admin/rules/assets/virtual_mf-exposes-ssr___mfe_internal__ActionVisu__customRuleBlocks_js-4sYoFXce.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./ActionVisu-FLuGw8uk.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./vite-preload-helper-B7qeedMF.js";var t={"./ActionVisu":async()=>{let t=await e(()=>import(`./ActionVisu-FLuGw8uk.js`),__vite__mapDeps([0]),import.meta.url),n={};return Object.assign(n,t),Object.defineProperty(n,"__esModule",{value:!0,enumerable:!1}),n}};export{t as default};
